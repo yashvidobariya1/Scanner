7d72906b96c0c036aab26773094c216d0aff627b
yashvidobariya1 <yashvishreesoftic@gmail.com>
2025-01-11 11:29:03 +0530

    first commit

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import QRScanner from "./QrScanner";
+
+function App() {
+  return (
+    <div className="App">
+      <QRScanner />
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/QrScanner.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Html5QrcodeScanner } from "html5-qrcode";
+
+const QRScanner = () => {
+  const [scanResult, setScanResult] = useState("");
+
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    console.log("Camera access is available.");
+  } else {
+    console.error("Camera access is not supported on this device.");
+  }
+
+  const startScanner = () => {
+    const qrCodeScanner = new Html5QrcodeScanner(
+      "reader",
+      { fps: 10, qrbox: 250 },
+      false
+    );
+    qrCodeScanner.render(
+      (result) => {
+        setScanResult(result.text);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  };
+
+  return (
+    <div>
+      <h1>QR Scanner</h1>
+      <div id="reader" style={{ width: "100%", height: "400px" }}></div>
+      <button onClick={startScanner}>Start Scanning</button>
+      <p>Scanned Data: {scanResult}</p>
+    </div>
+  );
+};
+
+export default QRScanner;
